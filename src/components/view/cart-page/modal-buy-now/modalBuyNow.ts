a7b1145d17f5b0fac682763af26962b7c96f608d
Alexandr <tribisalexandr@yandex.ru>
2023-01-08 00:26:50 +0300

    feat: add validate from modal form

--- src/components/view/cart-page/modal-buy-now/modalBuyNow.ts
+++ src/components/view/cart-page/modal-buy-now/modalBuyNow.ts
@@ -17,7 +17,6 @@ class modalBuyNow extends DomElement {
 
   public draw(): HTMLElement {
     this.element.innerHTML = '';
-
     const modal: HTMLElement = this.createElement('div', 'modal-dialog w-100');
     const modalContent: HTMLElement = this.createElement('div', 'modal-content');
     const modalHeader: HTMLElement = this.createElement('div', 'modal-header');
@@ -28,36 +27,41 @@ class modalBuyNow extends DomElement {
     const modalHeaderName: HTMLElement = this.createElement(
       'h5',
       'modal-title fs-4',
-      { id: 'staticBackdropLabel' },
+      {
+        id: 'staticBackdropLabel',
+        pattern: '^\\S+ \\S+$',
+      },
       'PERSONAL DETAILS'
     );
     const modalHeaderButtonClose: HTMLElement = this.createElement('button', 'btn-close', {
       'data-bs-dismiss': 'modal',
       'aria-label': 'Close',
     });
+
     const modalForm: HTMLElement = this.createElement(
       'form',
-      'd-flex m-2 w-75 fs-5 flex-column align-items-start justify-content-start'
+      'd-flex m-1 w-100 fs-5 flex-column align-items-start justify-content-start needs-validation'
     );
 
-    const modalFormName: HTMLElement = this.createElement('div', 'form-floating mb-3');
-    const modalFormNameInput: HTMLElement = this.createElement('input', 'form-control', {
+    const modalFormName: HTMLElement = this.createElement('div', 'form-floating mb-3 w-100');
+    const modalFormNameInput: HTMLElement = this.createElement('input', 'field form-control', {
       type: 'text',
-      pattern: '[a-z]{1,15}',
-      required: '',
       id: 'name-input',
       placeholder: 'Name',
     });
+    const modalNameValid: HTMLElement = this.createElement('div', 'd-none', undefined, 'Ok');
+    const modalNameInValid: HTMLElement = this.createElement('div', '', undefined, '');
     const modalFormNameLabel: HTMLElement = this.createElement('label', '', { for: 'name-input' }, 'Name');
 
-    const modalFormPhone: HTMLElement = this.createElement('div', 'form-floating mb-3');
-    const modalFormPhoneInput: HTMLElement = this.createElement('input', 'form-control', {
+    const modalFormPhone: HTMLElement = this.createElement('div', 'form-floating mb-3 w-100');
+    const modalFormPhoneInput: HTMLElement = this.createElement('input', 'field form-control', {
       type: 'tel',
-      required: true,
-      // pattern: `'\s{0,}\+{1,1}375\s{0,}\({0,1}(([2]{1}([5]{1}|[9]{1}))|([3]{1}[3]{1})|([4]{1}[4]{1}))\)\s{0,}[0-9]{3,3}\s{0,}[0-9]{4,4}'`,
+      minlength: '10',
+      // required: 'required',
       id: 'phone-number-input',
       placeholder: 'Phone number',
     });
+    const modalPhoneInValid: HTMLElement = this.createElement('div', '', undefined, '');
     const modalFormPhoneLabel: HTMLElement = this.createElement(
       'label',
       '',
@@ -65,13 +69,14 @@ class modalBuyNow extends DomElement {
       'Phone number'
     );
 
-    const modalFormAddress: HTMLElement = this.createElement('div', 'form-floating mb-3');
-    const modalFormAddressInput: HTMLElement = this.createElement('input', 'form-control', {
+    const modalFormAddress: HTMLElement = this.createElement('div', 'form-floating mb-3 w-100');
+    const modalFormAddressInput: HTMLElement = this.createElement('input', 'field form-control', {
       type: 'text',
-      required: true,
+      // required: 'required',
       id: 'address-input',
       placeholder: 'Delivery address',
     });
+    const modalAddressInValid: HTMLElement = this.createElement('div', '', undefined, '');
     const modalFormAddressLabel: HTMLElement = this.createElement(
       'label',
       '',
@@ -79,28 +84,29 @@ class modalBuyNow extends DomElement {
       'Delivery address'
     );
 
-    const modalFormEmail: HTMLElement = this.createElement('div', 'form-floating mb-3');
-    const modalFormEmailInput: HTMLElement = this.createElement('input', 'form-control', {
+    const modalFormEmail: HTMLElement = this.createElement('div', 'form-floating mb-3 w-100');
+    const modalFormEmailInput: HTMLElement = this.createElement('input', 'field form-control', {
       type: 'email',
-      required: true,
+      // required: 'required',
       id: 'email-input',
       placeholder: 'name@example.com',
     });
-
+    const modalEmailInValid: HTMLElement = this.createElement('div', '', undefined, '');
     const modalFormEmailLabel: HTMLElement = this.createElement('label', '', { for: 'email-input' }, 'Email address');
 
     const modalFormCreditCard: HTMLElement = this.createElement(
       'div',
-      'card-bock p-3 border border-start p-2 rounded-4'
+      'card-bock p-3 border border-start p-2 rounded-4 mb-2'
     );
 
     const modalFormCreditCardNumber: HTMLElement = this.createElement('div', 'form-floating mb-3 me-2');
-    const modalFormCreditCardNumberInput: HTMLElement = this.createElement('input', 'form-control', {
+    const modalFormCreditCardNumberInput: HTMLElement = this.createElement('input', 'field form-control', {
       type: 'text',
-      required: true,
+      // required: 'required',
       id: 'card-number-input',
       placeholder: 'Credit card number',
     });
+    const modalCardNumberInValid: HTMLElement = this.createElement('div', '', undefined, '');
     const modalFormCreditCardNumberLabel: HTMLElement = this.createElement(
       'label',
       '',
@@ -111,21 +117,23 @@ class modalBuyNow extends DomElement {
     const modalFormCreditCardDC: HTMLElement = this.createElement('div', 'd-flex');
 
     const modalFormCreditCardDate: HTMLElement = this.createElement('div', 'form-floating mb-3 me-2');
-    const modalFormCreditCardDateInput: HTMLElement = this.createElement('input', 'form-control', {
+    const modalFormCreditCardDateInput: HTMLElement = this.createElement('input', 'field form-control', {
       type: 'text',
-      required: true,
+      // required: 'required',
       id: 'date-input',
       placeholder: 'Data',
     });
+    const modalCardDateInValid: HTMLElement = this.createElement('div', '', undefined, '');
     const modalFormCreditCardDateLabel: HTMLElement = this.createElement('label', '', { for: 'date-input' }, 'Data');
 
     const modalFormCreditCardCVV: HTMLElement = this.createElement('div', 'form-floating mb-3');
-    const modalFormCreditCardCVVInput: HTMLElement = this.createElement('input', 'form-control', {
+    const modalFormCreditCardCVVInput: HTMLElement = this.createElement('input', 'field form-control', {
       type: 'text',
-      required: '',
+      // required: 'required',
       id: 'code-input',
       placeholder: 'Code',
     });
+    const modalCardCVVInValid: HTMLElement = this.createElement('div', '', undefined, '');
     const modalFormCreditCardCVVLabel: HTMLElement = this.createElement('label', '', { for: 'code-input' }, 'CVV');
 
     const modalFooter: HTMLElement = this.createElement('div', 'modal-footer');
@@ -142,14 +150,87 @@ class modalBuyNow extends DomElement {
     const modalFooterBuyButton: HTMLElement = this.createElement(
       'button',
       'btn btn-warning',
-      { type: 'button', 'data-bs-dismiss': 'modal' },
+      { type: 'submit' },
       'BUY NOW'
     );
 
-    modalFormNameInput.addEventListener('input', (e: Event) => {
-      const currentText = (<HTMLInputElement>e.target).value;
-      console.log(currentText);
+    // modalFormNameInput.addEventListener('input', (e: Event) => {
+    //   const currentText = (<HTMLInputElement>e.target).value;
+    //   console.log(currentText);
+    // });
+
+    modalForm.addEventListener('submit', function (event: Event) {
+      validateForm();
+      event.preventDefault();
     });
+
+    function printError(elemId: HTMLElement, hintMsg: string) {
+      elemId.textContent = hintMsg;
+    }
+
+    function validateForm() {
+      let nameErr = true;
+      let phoneErr = true;
+      let addressErr = true;
+      let emailErr = true;
+      const cardNumErr = true;
+      const cardDatErr = true;
+      const cardCvvErr = true;
+
+      if ((<HTMLInputElement>modalFormNameInput).value == '') {
+        printError(modalNameInValid, 'Please, enter your name');
+      } else {
+        const regex = /^([A-Z]{1}[a-z]{4,}\s[A-Z]{1}[a-z]{4,}([A-Z]{1}[a-z]{4,})?)/;
+        if (regex.test((<HTMLInputElement>modalFormNameInput).value) === false) {
+          printError(modalNameInValid, '2 words name, space, capital letters');
+        } else {
+          printError(modalNameInValid, '');
+          nameErr = false;
+        }
+      }
+
+      if ((<HTMLInputElement>modalFormPhoneInput).value === '') {
+        printError(modalPhoneInValid, 'Please, enter your phone number');
+      } else {
+        const regex = /^\+[1-9]\d{9}$/;
+        if (regex.test((<HTMLInputElement>modalFormPhoneInput).value) === false) {
+          printError(modalPhoneInValid, 'Please, enter a valid 9-digit phone number starting with the + sign');
+        } else {
+          printError(modalPhoneInValid, '');
+          phoneErr = false;
+        }
+      }
+
+      if ((<HTMLInputElement>modalFormAddressInput).value == '') {
+        printError(modalAddressInValid, 'Please, enter your address');
+      } else {
+        const regex = /^([a-zA-Z]{5,}\s[a-zA-Z]{5,}\s[a-zA-Z]{5,}\s([a-zA-Z]{5,})?)/;
+        if (regex.test((<HTMLInputElement>modalFormAddressInput).value) === false) {
+          printError(modalAddressInValid, '3 words name, space');
+        } else {
+          printError(modalAddressInValid, '');
+          addressErr = false;
+        }
+      }
+
+      if ((<HTMLInputElement>modalFormEmailInput).value === '') {
+        printError(modalEmailInValid, 'Please, enter your email address');
+      } else {
+        const regex = /^\S+@\S+\.\S+$/;
+        if (regex.test((<HTMLInputElement>modalFormEmailInput).value) === false) {
+          printError(modalEmailInValid, 'Please, enter a valid email address');
+        } else {
+          printError(modalEmailInValid, '');
+          emailErr = false;
+        }
+      }
+
+      if ((nameErr || phoneErr || addressErr || emailErr || cardNumErr || cardDatErr || cardCvvErr) === true) {
+        return false;
+      } else {
+        alert("It's ok");
+      }
+    }
 
     this.element.appendChild(modal);
     modal.appendChild(modalContent);
@@ -161,39 +242,37 @@ class modalBuyNow extends DomElement {
     modalForm.appendChild(modalFormName);
     modalFormName.appendChild(modalFormNameInput);
     modalFormName.appendChild(modalFormNameLabel);
+    modalFormName.appendChild(modalNameValid);
+    modalFormName.appendChild(modalNameInValid);
     modalForm.appendChild(modalFormPhone);
     modalFormPhone.appendChild(modalFormPhoneInput);
     modalFormPhone.appendChild(modalFormPhoneLabel);
+    modalFormPhone.appendChild(modalPhoneInValid);
     modalForm.appendChild(modalFormAddress);
     modalFormAddress.appendChild(modalFormAddressInput);
     modalFormAddress.appendChild(modalFormAddressLabel);
+    modalFormAddress.appendChild(modalAddressInValid);
     modalForm.appendChild(modalFormEmail);
     modalFormEmail.appendChild(modalFormEmailInput);
     modalFormEmail.appendChild(modalFormEmailLabel);
+    modalFormEmail.appendChild(modalEmailInValid);
     modalForm.appendChild(modalFormCreditCard);
     modalFormCreditCard.appendChild(modalFormCreditCardNumber);
     modalFormCreditCardNumber.appendChild(modalFormCreditCardNumberInput);
     modalFormCreditCardNumber.appendChild(modalFormCreditCardNumberLabel);
+    modalFormCreditCardNumber.appendChild(modalCardNumberInValid);
     modalFormCreditCard.appendChild(modalFormCreditCardDC);
     modalFormCreditCardDC.appendChild(modalFormCreditCardDate);
     modalFormCreditCardDate.appendChild(modalFormCreditCardDateInput);
     modalFormCreditCardDate.appendChild(modalFormCreditCardDateLabel);
+    modalFormCreditCardDate.appendChild(modalCardDateInValid);
     modalFormCreditCardDC.appendChild(modalFormCreditCardCVV);
     modalFormCreditCardCVV.appendChild(modalFormCreditCardCVVInput);
     modalFormCreditCardCVV.appendChild(modalFormCreditCardCVVLabel);
+    modalFormCreditCardCVV.appendChild(modalCardCVVInValid);
     modalForm.appendChild(modalFooter);
     modalFooter.appendChild(modalFooterCloseButton);
     modalFooter.appendChild(modalFooterBuyButton);
-
-    // data.products.forEach((item) => {
-    //   let inCart = 0;
-    //   cart.forEach((e) => {
-    //     if (e.id === item.id) inCart = e.count;
-    //   });
-    //   const wrapper: HTMLElement = this.createElement('div', 'products-block__item');
-    //   // productsItems.appendChild(wrapper);
-    //   wrapper.appendChild(new ProductCard(item, inCart).drawGridView());
-    // });
 
     return this.element;
   }
