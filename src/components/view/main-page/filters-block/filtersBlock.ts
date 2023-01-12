f71808cdeb4db720fb1a21bf830432aafba0b985
ilya <shakurov.ia@gmail.com>
2023-01-12 14:15:37 +0300

    refactor: add types to filtersBlock.ts

--- src/components/view/main-page/filters-block/filtersBlock.ts
+++ src/components/view/main-page/filters-block/filtersBlock.ts
@@ -1,7 +1,7 @@
 import DomElement from '../../domElement';
 import * as noUiSlider from 'nouislider';
 import Filter from '../../../controller/filter';
-import type { ProductsData, QueryParams } from '../../../../types/types';
+import type { ProductsData, Product, QueryParams } from '../../../../types/types';
 
 class FiltersBlock extends DomElement {
   filter: Filter;
@@ -33,11 +33,11 @@ class FiltersBlock extends DomElement {
     this.element.innerHTML = '';
 
     // Draw buttons
-    const buttonsWrapper = this.createElement('div', 'filters-block__buttons row mb-3');
-    const buttonColRes = this.createElement('div', 'col');
-    const buttonGridRes = this.createElement('div', 'd-grid gap-2');
-    const buttonColCopy = this.createElement('div', 'col');
-    const buttonGridCopy = this.createElement('div', 'd-grid gap-2');
+    const buttonsWrapper: HTMLElement = this.createElement('div', 'filters-block__buttons row mb-3');
+    const buttonColRes: HTMLElement = this.createElement('div', 'col');
+    const buttonGridRes: HTMLElement = this.createElement('div', 'd-grid gap-2');
+    const buttonColCopy: HTMLElement = this.createElement('div', 'col');
+    const buttonGridCopy: HTMLElement = this.createElement('div', 'd-grid gap-2');
 
     this.resetBtn.textContent = 'Reset filters';
     this.copyLInkBtn.textContent = 'Copy link';
@@ -51,8 +51,8 @@ class FiltersBlock extends DomElement {
     this.element.appendChild(buttonsWrapper);
 
     // Draw Price
-    const priceWrapper = this.createElement('div', 'filters-block__price mb-4');
-    const priceHeader = this.createElement('h5', 'filters-block__price-header mb-5', {}, 'Price');
+    const priceWrapper: HTMLElement = this.createElement('div', 'filters-block__price mb-4');
+    const priceHeader: HTMLElement = this.createElement('h5', 'filters-block__price-header mb-5', {}, 'Price');
 
     this.element.appendChild(priceWrapper);
     priceWrapper.appendChild(priceHeader);
@@ -61,8 +61,8 @@ class FiltersBlock extends DomElement {
     this.drawRangeFilter(data, this.priceFilter, 'price', params);
 
     // Draw Category
-    const categoryWrapper = this.createElement('div', 'filters-block__category mb-4');
-    const categoryHeader = this.createElement('h5', 'filters-block__category-header', {}, 'Category');
+    const categoryWrapper: HTMLElement = this.createElement('div', 'filters-block__category mb-4');
+    const categoryHeader: HTMLElement = this.createElement('h5', 'filters-block__category-header', {}, 'Category');
 
     this.element.appendChild(categoryWrapper);
     categoryWrapper.appendChild(categoryHeader);
@@ -71,8 +71,8 @@ class FiltersBlock extends DomElement {
     this.drawCheckboxFilter(data, this.categoryFilter, 'category', params);
 
     // Draw Brand
-    const brandWrapper = this.createElement('div', 'filters-block__brand mb-4');
-    const brandHeader = this.createElement('h5', 'filters-block__brand-header', {}, 'Brand');
+    const brandWrapper: HTMLElement = this.createElement('div', 'filters-block__brand mb-4');
+    const brandHeader: HTMLElement = this.createElement('h5', 'filters-block__brand-header', {}, 'Brand');
 
     this.element.appendChild(brandWrapper);
     brandWrapper.appendChild(brandHeader);
@@ -81,8 +81,8 @@ class FiltersBlock extends DomElement {
     this.drawCheckboxFilter(data, this.brandFilter, 'brand', params);
 
     // Draw Stock
-    const stockWrapper = this.createElement('div', 'filters-block__stock mb-4');
-    const stockHeader = this.createElement('h5', 'filters-block__stock-header mb-5', {}, 'Stock');
+    const stockWrapper: HTMLElement = this.createElement('div', 'filters-block__stock mb-4');
+    const stockHeader: HTMLElement = this.createElement('h5', 'filters-block__stock-header mb-5', {}, 'Stock');
 
     this.element.appendChild(stockWrapper);
     stockWrapper.appendChild(stockHeader);
@@ -110,7 +110,7 @@ class FiltersBlock extends DomElement {
     let min: number = midData;
     let max: number = midData;
 
-    const filteredData = this.filter.filterData(data, params);
+    const filteredData: Product[] = this.filter.filterData(data, params);
     if (filteredData.length) {
       min = filteredData.reduce((pr, cu) => (cu[filter] < pr[filter] ? cu : pr), filteredData[0])[filter];
       max = filteredData.reduce((pr, cu) => (cu[filter] > pr[filter] ? cu : pr), filteredData[0])[filter];
@@ -162,22 +162,22 @@ class FiltersBlock extends DomElement {
   ): void {
     filterElement.innerHTML = '';
 
-    let allFiltersArr = data.products.map((e) => e[filter]);
+    let allFiltersArr: string[] = data.products.map((e) => e[filter]);
     allFiltersArr = [...new Set(allFiltersArr)];
     const checkedArr: string[] = params[filter]?.split('%');
-    const filteredData = this.filter.filterData(data, params);
+    const filteredData: Product[] = this.filter.filterData(data, params);
 
     allFiltersArr.forEach((item) => {
-      const wrapper = this.createElement('div', 'checkbox-filter d-flex form-check');
+      const wrapper: HTMLElement = this.createElement('div', 'checkbox-filter d-flex form-check');
       const input: HTMLInputElement = <HTMLInputElement>(
         this.createElement('input', 'checkbox-filter__cb form-check-input', { type: 'checkbox', value: item, id: item })
       );
-      const label = this.createElement('label', 'form-check-label', { for: item }, item);
+      const label: HTMLElement = this.createElement('label', 'form-check-label', { for: item }, item);
 
-      const foundItems = filteredData.filter((e) => e[filter] === item).length;
-      const totalItems = data.products.filter((e) => e[filter] === item).length;
+      const foundItems: number = filteredData.filter((e) => e[filter] === item).length;
+      const totalItems: number = data.products.filter((e) => e[filter] === item).length;
 
-      const countItemsElement = this.createElement('span', 'checkbox-filter__count flex-fill');
+      const countItemsElement: HTMLElement = this.createElement('span', 'checkbox-filter__count flex-fill');
       countItemsElement.textContent = `${foundItems}/${totalItems}`;
 
       if (checkedArr && checkedArr.includes(item)) {
